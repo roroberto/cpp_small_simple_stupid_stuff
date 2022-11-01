4e785f89ede21a01946ea44b6219bfb4622892f1
roroberto <roberto.romani@gmail.com>
2022-11-01 21:07:30 +0100

    Review names of concepts

--- value_or/value_or/value_or.h
+++ value_or/value_or/value_or.h
@@ -25,30 +25,30 @@ namespace s4 // Small Simple Stupid Stuff namespace
 {
 
     /**
-     * Concept that define a pointer of type PointerType that points to
+     * Concept that defines an object that hold a value of type
      * ValueType. ValueType can be also an invocable, this is 
      * needed if the default value is an invocable.
      * It covers: raw pointers, std::unique_ptr, std:shared_ptr,
-     * std::weak_ptr, std::optional, std::null_ptr, and any other
-     * class that has the operators * and bool.
+     * std::weak_ptr, std::optional, std::nullptr, and any other
+     * class that has the operators * and !.
      */
-    template<typename PointerType, typename ValueType>
-    concept value_or_pointer_to = 
+    template<typename ValueHolderType, typename ValueType>
+    concept value_or_value_holder = 
     (   std::invocable<ValueType>
         &&
         ( 
-            // Pointer must behave like a raw pointer to an object of class 
+            // value_holder must behave like a raw pointer to an object of class 
             // decltype(value())
-            requires(PointerType pointer, ValueType value)  
+            requires(ValueHolderType value_holder, ValueType value)  
             {
-                {!pointer};
-                {*pointer} -> std::convertible_to<decltype(value())>;
+                {!value_holder};
+                {*value_holder} -> std::convertible_to<decltype(value())>;
             }
-            // OR Pointer must behave like a weak_ptr that points to an object of 
+            // OR value_holder must behave like a weak_ptr that points to an object of 
             // class decltype(value())
-            || requires(PointerType pointer, ValueType value) 
+            || requires(ValueHolderType value_holder, ValueType value)
             {
-                {*pointer.lock()} -> std::convertible_to<decltype(value())>; 
+                {*value_holder.lock()} -> std::convertible_to<decltype(value())>;
             }
         )
     )
@@ -56,60 +56,60 @@ namespace s4 // Small Simple Stupid Stuff namespace
     (   !std::invocable<ValueType>
         && 
         (
-            // Pointer must behave like a raw pointer that points to a ValueType
-            requires(PointerType pointer, ValueType)  
+            // value_holder must behave like a raw pointer that points to a ValueType
+            requires(ValueHolderType value_holder, ValueType)
             {
-                {!pointer};
-                {*pointer} -> std::convertible_to<ValueType>;
+                {!value_holder};
+                {*value_holder} -> std::convertible_to<ValueType>;
             }
-            // OR Pointer must behave like a weak_ptr that points to a ValueType
-            || requires(PointerType pointer, ValueType) 
+            // OR value_holder must behave like a weak_ptr that points to a ValueType
+            || requires(ValueHolderType value_holder, ValueType)
             {
-                {*pointer.lock()} -> std::convertible_to<ValueType>;
+                {*value_holder.lock()} -> std::convertible_to<ValueType>;
             }
         )
     )
     // to cover the case of null_ptr
-    || std::same_as<std::nullptr_t, PointerType>; 
+    || std::same_as<std::nullptr_t, ValueHolderType>; 
 
 
     /**
-     * Concept that define the requirements of the value_or parameters, 
+     * Concept that defines the requirements of the value_or parameters, 
      * they can be pointers or functions that return pointers.
      */
-    template<typename PointerType, typename ValueType>
+    template<typename ValueHolderType, typename ValueType>
     concept value_or_param =
-    requires(PointerType callable, ValueType)
+    requires(ValueHolderType callable, ValueType)
     {
-        {callable()} -> value_or_pointer_to<ValueType>;
+        {callable()} -> value_or_value_holder<ValueType>;
     }
-    || value_or_pointer_to<PointerType, ValueType>;
+    || value_or_value_holder<ValueHolderType, ValueType>;
 
 
     namespace value_or_impl
     {
-        template<typename ValueType, typename PointerType>
-        concept raw_pointer_to = requires(ValueType, PointerType pointer)
+        template<typename ValueType, typename ValueHolderType>
+        concept raw_pointer_to = requires(ValueType, ValueHolderType pointer)
         {
             {*pointer} -> std::convertible_to<ValueType>;
             {!pointer};
         }
-        || requires(ValueType value, PointerType pointer)
+        || requires(ValueType value, ValueHolderType pointer)
         {
             {*pointer} -> std::convertible_to<decltype(value())>;
             {!pointer};
         };
 
-        template<typename ValueType, typename PointerType>
-        concept weak_pointer_to = requires(PointerType pointer, ValueType)
+        template<typename ValueType, typename ValueHolderType>
+        concept weak_pointer_to = requires(ValueHolderType pointer, ValueType)
         {
             {*pointer.lock()} noexcept -> std::convertible_to<ValueType>;
         };
 
-        template<typename ValueType, typename PointerType>
-        concept callable_ptr_to = requires(PointerType callable, ValueType)
+        template<typename ValueType, typename ValueHolderType>
+        concept callable_ptr_to = requires(ValueHolderType callable, ValueType)
         {
-            {callable()}  -> value_or_pointer_to<ValueType>;
+            {callable()}  -> value_or_value_holder<ValueType>;
         };
 
 
@@ -144,7 +144,6 @@ namespace s4 // Small Simple Stupid Stuff namespace
         template<typename RT, typename DT, typename PT, typename... Args>
         requires weak_pointer_to<RT, PT> 
         constexpr std::remove_reference_t<RT> value_or(DT&& default_value, PT&& to_test_0, Args&&... to_test_v);
-        
 
 
         /**
@@ -188,8 +187,9 @@ namespace s4 // Small Simple Stupid Stuff namespace
         constexpr decltype(auto) value_or(DT&& default_value, std::nullptr_t, Args&&... to_test_v);
 
 
+
         /**
-        * It just return the default_value, this function is used to end the recursion of value_or.
+        * It returns the default_value, this function is used to end the recursion of value_or.
         *
         * \param default_value The value to return
         * \return default_value
@@ -202,7 +202,13 @@ namespace s4 // Small Simple Stupid Stuff namespace
             return static_cast<RT>(default_value);
         }
 
-        
+        /**
+        * It returns default_value() in the case the default value is invocable. 
+        * this function is used to end the recursion of value_or.
+        *
+        * \param default_value The value to return
+        * \return default_value
+        */
         template<typename RT, typename DT>
         requires std::convertible_to< std::invoke_result_t<DT>, std::remove_reference_t<RT> >
             && std::invocable<DT>
@@ -210,6 +216,8 @@ namespace s4 // Small Simple Stupid Stuff namespace
         {
             return default_value();
         }
+
+                  
 
         template<typename RT, typename DT, typename PT, typename... Args>
         requires raw_pointer_to<RT, PT> 
@@ -223,6 +231,7 @@ namespace s4 // Small Simple Stupid Stuff namespace
                     : static_cast<RT>(*to_test_0);
         }
 
+        
 
         template<typename RT, typename DT, typename PT, typename... Args>
         requires weak_pointer_to<RT, PT> 
@@ -236,6 +245,7 @@ namespace s4 // Small Simple Stupid Stuff namespace
                     std::forward<Args>(to_test_v)...);
         }
 
+    
 
         template<typename RT, typename DT, typename PT, typename... Args>
         requires std::convertible_to< PT, std::remove_reference_t<RT> > 
@@ -285,6 +295,7 @@ namespace s4 // Small Simple Stupid Stuff namespace
                     std::forward<DT>(default_value),
                     std::forward<Args>(to_test_v)...);
         }
+
     }
 
     /**
@@ -299,7 +310,7 @@ namespace s4 // Small Simple Stupid Stuff namespace
     */
     template<typename ReturnType, typename DefaultType, value_or_param<ReturnType>... Args>
     requires std::convertible_to< DefaultType, std::remove_reference_t<ReturnType> >
-    constexpr decltype(auto) value_or(DefaultType&& default_value, Args&&... to_test_v)
+    constexpr ReturnType value_or(DefaultType&& default_value, Args&&... to_test_v)
     {
         return s4::value_or_impl::value_or<ReturnType, DefaultType, Args...>(
             std::forward<DefaultType>(default_value),
@@ -331,7 +342,7 @@ namespace s4 // Small Simple Stupid Stuff namespace
                 std::forward<DefaultType>(default_value),
                 std::forward<Args>(to_test_v)...) ;
     }
-    
+  
 } // end namespace s4
 
 #endif
